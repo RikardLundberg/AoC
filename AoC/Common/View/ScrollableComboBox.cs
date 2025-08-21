a33e3b68da8a4b12a0beb35b4f5ff7cbd49dc492
RikardLundberg <rikardlundberg90@gmail.com>
2025-08-21 14:13:49 +0200

    Added custom comboboxes that scroll on hover

---
+++ AoC/Common/View/ScrollableComboBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace AoC.Common.View
+{
+    public class ScrollableComboBox : ComboBox
+    {
+        public ScrollableComboBox() 
+        {
+            MouseEnter += ScrollableComboBox_MouseEnter;
+        }
+
+        private void ScrollableComboBox_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Focus();
+        }
+    }
+}
