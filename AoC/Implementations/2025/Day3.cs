87296fed201bcac5e097468fc7f757fd616e9ffc
RikardLundberg <rikardlundberg90@gmail.com>
2025-12-03 08:46:29 +0100

    day3 2025 first star

---
+++ AoC/Implementations/2025/Day3.cs
@@ -0,0 +1,59 @@
+﻿namespace AoC.Implementations._2025
+{
+    internal class Day3 : IDay
+    {
+        private string _result { get; set; } = string.Empty;
+        private bool secondPart { get; set; } = true; //should be triggered in UI
+
+        public string GetResult()
+        {
+            return _result;
+        }
+
+        public void Run(string puzzleData)
+        {
+            var total = 0;
+            foreach (var bank in puzzleData.Split("\n"))
+            {
+                total += GetLargestFromBank(bank);
+            }
+            _result = total.ToString();
+        }
+
+        private int GetLargestFromBank(string bank)
+        {
+            var highestJoltage = "";
+            var currentHighest = '0';
+            int start = 0;
+            for (int i = 0; i < bank.Length - 1; i++)
+            {
+                if (bank[i] > currentHighest)
+                {
+                    currentHighest = bank[i];
+                    start = i + 1;
+                }
+                if (currentHighest == '9')
+                    break;
+            }
+            highestJoltage += currentHighest;
+            currentHighest = '0';
+            for (int i = start; i < bank.Length; i++)
+            {
+                if (bank[i] > currentHighest)
+                {
+                    currentHighest = bank[i];
+                }
+                if (currentHighest == '9')
+                    break;
+            }
+            highestJoltage += currentHighest;
+            return int.Parse(highestJoltage);
+        }
+
+        public void TestRun()
+        {
+            string input = "987654321111111\n811111111111119\n234234234234278\n818181911112111";
+            Run(input);
+        }
+    }
+}
