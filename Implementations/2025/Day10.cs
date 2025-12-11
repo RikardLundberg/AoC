c8853b051cee06ba20f6f8a819cb9af4f7cf4496
RikardLundberg <rikardlundberg90@gmail.com>
2025-12-11 11:26:50 +0100

    day9+10 wip 2025

---
+++ Implementations/2025/Day10.cs
@@ -0,0 +1,150 @@
+﻿namespace Implementations._2025
+{
+    internal class Day10 : Day
+    {
+        public override void Run(string puzzleData)
+        {
+            var machines = ReadMachines(puzzleData);
+
+            var total = 0;
+            foreach (var machine in machines)
+            {
+                total += machine.FindShortestStartSequence();
+            }
+            _result = total.ToString();
+        }
+
+        private List<Machine> ReadMachines(string puzzleData)
+        {
+            var machines = new List<Machine>();
+            foreach (var line in puzzleData.Split('\n'))
+            {
+                var machine = new Machine();
+                machine.ReadManual(line);
+                machines.Add(machine);
+            }
+            return machines;
+        }
+
+        public override void TestRun()
+        {
+            string input = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\n[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\n[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}";
+            Run(input);
+        }
+    }
+
+    internal class Machine
+    {
+        public List<bool> StartSequence { get; set; } = new List<bool>();
+        public List<List<int>> Buttons { get; set; } = new List<List<int>>();
+        public List<int> JoltageRequirements { get; set; } = new List<int>();
+
+        public Machine()
+        {
+            StartSequence = new List<bool>();
+            Buttons = new List<List<int>>();
+            JoltageRequirements = new List<int>();
+        }
+
+        public int FindShortestStartSequence()
+        {
+            for (int i = 1; i < Buttons.Count; i++) //it does not do us much good to press same button twice
+            {
+                if (TryAllCombinations(i, 0, new List<int>()) != -1)
+                    return i;
+            }
+            throw new Exception("Unsolvable");
+        }
+
+        private int TryAllCombinations(int buttonCount, int index, List<int> pressedButtons)
+        {
+            if (StartSequenceMatches(pressedButtons) != -1)
+                return pressedButtons.Count;
+            if (index >= Buttons.Count)
+                return -1;
+            for (int i = pressedButtons.Count; i < buttonCount; i++)
+            {
+                if (pressedButtons.Contains(i))
+                    continue;
+                pressedButtons.Add(i);
+                var val = TryAllCombinations(buttonCount, index + 1, pressedButtons);
+                if (val != -1) return val;
+                pressedButtons.RemoveAt(pressedButtons.Count - 1);
+            }
+            return -1;
+        }
+
+        private int StartSequenceMatches(List<int> pressedButtons)
+        {
+            var startSequenceTmp = new List<bool>(this.StartSequence);
+            foreach (var button in pressedButtons)
+            {
+                for (int i = 0; i < Buttons[button].Count; i++)
+                {
+                    int position = Buttons[button][i];
+                    if (position >= startSequenceTmp.Count)
+                        return -1;
+                    startSequenceTmp[position] = !startSequenceTmp[position];
+                }
+            }
+            if (startSequenceTmp == StartSequence)
+                return pressedButtons.Count;
+            return -1;
+        }
+
+        public void ReadManual(string manual)
+        {
+            foreach (var segment in manual.Split(' '))
+            {
+                ReadManualSegment(segment);
+            }
+        }
+
+        private void ReadManualSegment(string segment)
+        {
+            if (segment.StartsWith("[") && segment.EndsWith("]"))
+            {
+                ReadStartSequence(segment.Trim('[', ']'));
+            }
+            else if (segment.StartsWith("(") && segment.EndsWith(")"))
+            {
+                ReadButton(segment.Trim('(', ')'));
+            }
+            else if (segment.StartsWith("{") && segment.EndsWith("}"))
+            {
+                ReadJoltageRequirements(segment);
+            }
+        }
+
+        private void ReadJoltageRequirements(string data)
+        {
+            var parts = data.Trim('{', '}').Split(',');
+            foreach (var part in parts)
+            {
+                if (int.TryParse(part, out int value))
+                {
+                    JoltageRequirements.Add(value);
+                }
+            }
+        }
+
+        private void ReadStartSequence(string data)
+        {
+            foreach (var ch in data)
+            {
+                StartSequence.Add(ch == '#' ? true : false);
+            }
+        }
+
+        private void ReadButton(string data)
+        {
+            var buttonSequence = new List<int>();
+            foreach (var buttonIndex in data.Split(','))
+            {
+                if (int.TryParse(buttonIndex, out int result))
+                    buttonSequence.Add(result);
+            }
+            Buttons.Add(buttonSequence);
+        }
+    }
+}
--- Implementations/2025/Day9.cs
+++ Implementations/2025/Day9.cs
@@ -15,30 +15,162 @@ namespace Implementations._2025
                 points.Add(new Point(int.Parse(coords[0]), int.Parse(coords[1])));
             }
 
-            var largestArea = 0L;
+            var areas = new List<Area>();
             for (int i = 0; i < points.Count; i++)
             {
                 for (int j = i + 1; j < points.Count; j++)
                 {
-                    var area = CalculateArea(points[i], points[j]);
-                    if (area > largestArea)
-                        largestArea = area;
+                    areas.Add(CalculateArea(points[i], points[j]));
                 }
             }
-            _result = largestArea.ToString();
+
+            areas.Sort((a, b) => a.TotalArea.CompareTo(b.TotalArea));
+            areas.Reverse();
+
+            if (!_secondStar)
+                _result = areas[0].TotalArea.ToString();
+            else
+            {
+                var lines = new List<Vector>();
+                for (int i = 0; i < points.Count - 1; i++)
+                    lines.Add(Point.Subtract(points[i + 1], points[i]));
+
+                int tryCount = 0;
+                while (tryCount < areas.Count)
+                {
+                    var area = areas[tryCount];
+                    bool intersects = false;
+
+                    for (int i = 0; i < lines.Count; i++)
+                    {
+                        var line = lines[i];
+                        var areaLines = new List<Vector>()
+                        {
+                            new Vector(area.Second.X - area.First.X, 0),
+                            new Vector(0, area.Second.Y - area.First.Y),
+                            new Vector(area.First.X - area.Second.X, 0),
+                            new Vector(0, area.First.Y - area.Second.Y)
+                        };
+                        //var areaLines = new List<Line2D>()
+                        //{
+                        //    new Line2D(new Point2D(area.First.X, area.First.Y), new Point2D(area.Second.X, area.First.Y)),
+                        //    new Line2D(new Point2D(area.Second.X, area.First.Y), new Point2D(area.Second.X,  area.Second.Y)),
+                        //    new Line2D(new Point2D(area.Second.X, area.Second.Y), new Point2D(area.First.X, area.Second.Y)),
+                        //    new Line2D(new Point2D(area.First.X, area.Second.Y), new Point2D(area.First.X, area.First.Y))
+                        //};
+                        foreach (var areaLine in areaLines)
+                        {
+                            if (line.IntersectsWith(areaLine))
+                            {
+                                intersects = true;
+                                break;
+                            }
+                        }
+                        if (intersects)
+                            break;
+                    }
+                    if (!intersects)
+                    {
+                        _result = area.TotalArea.ToString();
+                        return;
+                    }
+                    tryCount++;
+                }
+            }
         }
 
-        private long CalculateArea(Point a, Point b)
+        //private Point GetIntersection()
+        //{
+        //    return new Point();
+        //}
+
+        private Area CalculateArea(Point a, Point b)
         {
+            var area = 0L;
             var x = (long)Math.Abs(a.X - b.X) + 1;
             var y = (long)Math.Abs(a.Y - b.Y) + 1;
-            return x * y;
+            return new Area(a, b, x * y);
         }
 
         public override void TestRun()
         {
             string input = "7,1\n11,1\n11,7\n9,7\n9,5\n2,5\n2,3\n7,3";
             Run(input);
+        }
+    }
+
+    //internal class Point2D
+    //{
+    //    public int X { get; set; }
+    //    public int Y { get; set; }
+    //    public Point2D(int x, int y)
+    //    {
+    //        X = x; Y = y;
+    //    }
+    //}
+
+    //internal class Line2D
+    //{
+    //    public Point2D Start { get; set; }
+    //    public Point2D End { get; set; }
+    //    public double Slope { get; set; }
+    //    public Line2D(Point2D start, Point2D end)
+    //    {
+    //        Start = start; End = end;
+    //        Slope = (double)(end.Y - start.Y) / (end.X - start.X);
+    //    }
+    //}
+
+    internal static class Extensions2
+    {
+        public static bool IntersectsWith(this Vector first, Vector second)
+        {
+            var slope1 = first.Y / first.X;
+            var slope2 = second.Y / second.X;
+            if (slope1 == slope2)
+                return false;
+
+            //y = mx + b
+            var b1 = first.Y - (slope1 * first.X);
+            var b2 = second.Y - (slope2 * second.X);
+
+            var intersectX = (b2 - b1) / (slope1 - slope2);
+            var intersectY = (slope1 * intersectX) + b1;
+
+            if (intersectY == ((slope2 * intersectX) + b2))
+                return true;
+            return false;
+
+
+            //Vector3 lineVec3 = inLinePoint2 - inLinePoint1;
+            //Vector3 crossVec1and2 = Vector3.Cross(inLineVec1, inLineVec2);
+            //Vector3 crossVec3and2 = Vector3.Cross(lineVec3, inLineVec2);
+            //float planarFactor = Vector3.Dot(lineVec3, crossVec1and2);
+
+            //float dot = Vector3.Dot(crossVec3and2, crossVec1and2);
+            //float s = Vector3.Dot(crossVec3and2, crossVec1and2) / crossVec1and2.sqrMagnitude;
+            //inIntersection = inLinePoint1 + (inLineVec1 * s);
+            //return inIntersection;
+        }
+        //public static bool IntersectsWith(this Line2D first, Line2D second)
+        //{
+        //    if (Math.Abs(first.Slope).Equals(Math.Abs(second.Slope)))
+        //        return false;
+
+        //}
+
+    }
+
+    internal class Area
+    {
+        public Point First { get; set; }
+        public Point Second { get; set; }
+        public long TotalArea { get; set; }
+        public Area(Point first, Point second, long area)
+        {
+            this.First = first;
+            this.Second = second;
+            this.TotalArea = area;
         }
     }
 }
